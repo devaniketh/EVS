74953ab7d7f6d9f65e4240da2d387790e92df8d7
aniketh <techrunstheworld@gmail.com>
2025-09-27 21:50:00 +0530

    fix repo

--- client/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Hero from "@/components/sections/home-page/Hero";
-
-export default function Home() {
-  return (
-    <>
-      <Hero />
-    </>
-  );
-}
--- client/components/sections/home-page/Hero.tsx
+++
@@ -1,7 +0,0 @@
-export default function Hero() {
-  return (
-    <section>
-      <div className="container mx-auto font-serif">hello</div>
-    </section>
-  );
-}
